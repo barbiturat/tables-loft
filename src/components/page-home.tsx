ae9162ef07f9fdd4dead2eaa6a642f6814382503
vadimnekrasov <gdekefir@gmail.com>
2016-12-30 01:42:00 +0300

    fix in markup

--- src/components/page-home.tsx
+++ src/components/page-home.tsx
@@ -107,10 +107,11 @@ class PageHome extends React.Component<ComponentProps, AnyDict> {
           </div>
 
           <div className="table table_type_pool table_status_active tables-set_adjust_table">
-            <div className="table__label table__label_role_table-type">Pool Table 1</div>
+            <div className="table__label table__label_role_table-type">Tennis Table 3</div>
+            <div className="table__label table__label_role_start-time">20:30</div>
             <a href="" className="table__button table__button_role_change-availability"/>
-            <div className="table__label table__label_role_availability">
-              Available
+            <div className="table__label table__label_role_counter">
+              1h 30m 16s
             </div>
             <div className="table__session-info">
               <span className="table__session-name">Last Session</span>
@@ -135,10 +136,11 @@ class PageHome extends React.Component<ComponentProps, AnyDict> {
           </div>
 
           <div className="table table_type_default table_status_active tables-set_adjust_table">
-            <div className="table__label table__label_role_table-type">Default Table</div>
+            <div className="table__label table__label_role_table-type">Tennis Table 3</div>
+            <div className="table__label table__label_role_start-time">20:30</div>
             <a href="" className="table__button table__button_role_change-availability"/>
-            <div className="table__label table__label_role_availability">
-              Available
+            <div className="table__label table__label_role_counter">
+              1h 30m 16s
             </div>
             <div className="table__session-info">
               <span className="table__session-name">Last Session</span>
@@ -152,7 +154,7 @@ class PageHome extends React.Component<ComponentProps, AnyDict> {
             <span className="table__label table__label_role_disabled">
               Pool Table 2 Is Not Active
             </span>
-            <div className="table__label table__label_role_table-type">Pool Table 1</div>
+            <div className="table__label table__label_role_table-type">Tennis Table 1</div>
             <a href="" className="table__button table__button_role_change-availability"/>
             <div className="table__label table__label_role_availability">
               Available
@@ -169,7 +171,53 @@ class PageHome extends React.Component<ComponentProps, AnyDict> {
             <span className="table__label table__label_role_disabled">
               Pool Table 2 Is Not Active
             </span>
-            <div className="table__label table__label_role_table-type">Pool Table 1</div>
+            <div className="table__label table__label_role_table-type">Default Table 1</div>
+            <a href="" className="table__button table__button_role_change-availability"/>
+            <div className="table__label table__label_role_availability">
+              Available
+            </div>
+            <div className="table__session-info">
+              <span className="table__session-name">Last Session</span>
+              <span className="table__session-finish-time">20:30</span>
+              <span className="table__session-length">1h 10m</span>
+            </div>
+            <a href="" className="table__btn-view-sessions">View More</a>
+          </div>
+
+          <div className="table table_type_shuffle table_status_ready tables-set_adjust_table">
+            <div className="table__label table__label_role_table-type">Shuffle Board 5</div>
+            <a href="" className="table__button table__button_role_change-availability"/>
+            <div className="table__label table__label_role_availability">
+              Available
+            </div>
+            <div className="table__session-info">
+              <span className="table__session-name">Last Session</span>
+              <span className="table__session-finish-time">20:30</span>
+              <span className="table__session-length">1h 10m</span>
+            </div>
+            <a href="" className="table__btn-view-sessions">View More</a>
+          </div>
+
+          <div className="table table_type_shuffle table_status_active tables-set_adjust_table">
+            <div className="table__label table__label_role_table-type">Tennis Table 3</div>
+            <div className="table__label table__label_role_start-time">20:30</div>
+            <a href="" className="table__button table__button_role_change-availability"/>
+            <div className="table__label table__label_role_counter">
+              1h 30m 16s
+            </div>
+            <div className="table__session-info">
+              <span className="table__session-name">Last Session</span>
+              <span className="table__session-finish-time">20:30</span>
+              <span className="table__session-length">1h 10m</span>
+            </div>
+            <a href="" className="table__btn-view-sessions">View More</a>
+          </div>
+
+          <div className="table table_type_shuffle table_status_disabled tables-set_adjust_table">
+            <span className="table__label table__label_role_disabled">
+              Pool Table 2 Is Not Active
+            </span>
+            <div className="table__label table__label_role_table-type">Shuffle Board 5</div>
             <a href="" className="table__button table__button_role_change-availability"/>
             <div className="table__label table__label_role_availability">
               Available
