c933fcc101ff69e2a5866f3fa29f53b5106d0733
vadimnekrasov <gdekefir@gmail.com>
2017-01-04 13:10:10 +0300

    apply isSessionsHistoryInPending

--- src/epics/fetch-sessions-history.ts
+++ src/epics/fetch-sessions-history.ts
@@ -1,6 +1,7 @@
 import {Observable} from 'rxjs';
 import {Epic} from 'redux-observable';
 import {MiddlewareAPI} from 'redux';
+import {find} from 'lodash';
 
 import {FETCHING_TABLE_SESSIONS_HISTORY} from '../constants/action-names';
 import {get, getErrorMessageFromResponse} from '../helpers/requests';
@@ -14,40 +15,56 @@ import {urlSessionHistory} from '../constants/urls';
 import {SimpleAction} from '../interfaces/actions';
 import {tableSessionsToFront} from '../helpers/api-data-converters/index';
 import tableSessionsChanged from '../action-creators/table-sessions-changed';
-import pendingTableSessionsHistory from '../action-creators/pending-table-sessions-history';
 import fetchingTableSessionsHistoryFailed from '../action-creators/fetching-table-sessions-history-failed';
 import {ActionType} from '../action-creators/fetching-table-sessions-history';
 import {RequestSessionHistoryPayload} from '../interfaces/api-requests';
-import {StoreStructure} from '../interfaces/store-models';
+import {StoreStructure, Table} from '../interfaces/store-models';
+import tablesChanged from '../action-creators/tables-changed';
 
 type ResponseOk = AjaxResponseTyped<ResponseSessionsHistoryPayload>;
 type ResponseError = AjaxErrorTyped<ResponseFailedPayload>;
 
+const getTablesWithSetHistoryPending = (tables: Table[], tableId: number, isInPending: boolean): Table[] => {
+  const currentTable = find(tables, (table: Table) => {
+    return table.id === tableId;
+  });
+
+  if (currentTable) {
+    currentTable.isSessionsHistoryInPending = isInPending;
+  }
+
+  return tables;
+};
+
 const fetchSessionsHistory = ((action$, store: MiddlewareAPI<StoreStructure>) => {
   return action$.ofType(FETCHING_TABLE_SESSIONS_HISTORY)
     .switchMap((action: ActionType) => {
-      const historyPendingStart$ = Observable.of(pendingTableSessionsHistory(true));
       const tableId = action.payload;
       const dataToSend: RequestSessionHistoryPayload = {
         tableId
       };
+      const currentTablesClone: Table[] = store.getState().app.tablesData.tables.concat([]);
+      const tablesWithSetPending = getTablesWithSetHistoryPending(currentTablesClone, tableId, true);
+      const setTablesWithPending$ = Observable.of( tablesChanged(tablesWithSetPending) );
 
       const historyRequest$ = Observable.of(null)
         .mergeMap(() =>
           get(urlSessionHistory, dataToSend)
             .mergeMap((ajaxData: ResponseOk | ResponseError) => {
               if (ajaxData.status === STATUS_OK) {
+                const tablesClone = store.getState().app.tablesData.tables.concat([]);
                 const sessions = (ajaxData as ResponseOk).response.sessions;
                 const currentSessions = store.getState().app.tableSessionsData.tableSessions;
                 const convertedNewSessions = tableSessionsToFront(sessions);
                 const newSessions = currentSessions.concat(convertedNewSessions);
+                const tablesWithUnsetPending = getTablesWithSetHistoryPending(tablesClone, tableId, false);
 
                 const setTableSessionsAction = tableSessionsChanged(newSessions);
-                const historyPendingStartStopAction = pendingTableSessionsHistory(false);
+                const setTablesWithoutPendingAction = tablesChanged(tablesWithUnsetPending);
 
                 return Observable.of<any>(
-                  setTableSessionsAction,
-                  historyPendingStartStopAction
+                  setTablesWithoutPendingAction,
+                  setTableSessionsAction
                 );
               } else {
                 const errorMessage = getErrorMessageFromResponse(ajaxData as ResponseError);
@@ -60,7 +77,7 @@ const fetchSessionsHistory = ((action$, store: MiddlewareAPI<StoreStructure>) =>
         );
 
       return Observable.concat(
-        historyPendingStart$,
+        setTablesWithPending$,
         historyRequest$
       );
     });
