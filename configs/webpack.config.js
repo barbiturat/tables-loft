3e15a9d7a6641c8e6302ad3711c177c752e84e21
vadimnekrasov <gdekefir@gmail.com>
2017-01-12 23:55:03 +0300

    add build step to the postinstall

--- configs/webpack.config.js
+++ configs/webpack.config.js
@@ -1,11 +1,6 @@
 const webpack = require('webpack');
 const path = require('path');
-const autoprefixer = require('autoprefixer');
-const appSettings = require('../package.json').appSettings;
 
-const ROLLBAR_TOKEN = process.env.ROLLBAR_TOKEN || '';
-const isProd = process.argv.includes('-p');
-const nodeEnv = isProd ? 'production' : 'development';
 const sourcePath = pathFromRoot('./src');
 const outputPath = pathFromRoot('./public/resources');
 
@@ -13,97 +8,13 @@ function pathFromRoot(url = '') {
   return path.resolve(__dirname, '..', url);
 }
 
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      ROLLBAR_TOKEN: JSON.stringify(ROLLBAR_TOKEN),
-      NODE_ENV: JSON.stringify(nodeEnv)  // NODE_ENV: '"production"' for decreasing size of react library
-    }
-  }),
-  new webpack.LoaderOptionsPlugin({
-    test: /\.scss$/,
-    options: {
-      context: sourcePath,
-      debug: true,
-      postcss: () => [
-        autoprefixer({
-          browsers: ['> 1%', 'IE 7']
-        })
-      ]
-    }
-  })
-];
-
 module.exports = {
-  devtool: isProd ? 'source-map' : 'eval',
   context: sourcePath,
   entry: {
-    rollbar: './external-tools/rollbar-snippet.js',
-    bundle: './index.tsx'
+    rollbar: './external-tools/rollbar-snippet.js'
   },
   output: {
     filename: '[name].js',
     path: outputPath
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        loader: 'ts-loader',
-        options: {
-          configFileName: pathFromRoot('./configs/tsconfig.json')
-        }
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|otf|svg)$/,
-        loader: 'url-loader',
-        options: {
-          limit: 100000
-        }
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          'style-loader',
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 1
-            }
-          },
-          'postcss-loader',
-          {
-            loader: 'sass-loader',
-            options: {
-              includePaths: [
-                pathFromRoot('./node_modules')
-              ]
-            }
-          }
-        ]
-      }
-    ]
-  },
-  resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx'],
-    modules: [
-      sourcePath,
-      'node_modules'
-    ]
-  },
-  plugins: plugins,
-  watchOptions: {
-    aggregateTimeout: 300,
-    ignored: 'node_modules'
-  },
-  devServer: {
-    contentBase: './public',
-    historyApiFallback: true,
-    port: appSettings.devServerPort,
-    proxy: {
-      '/v1': `http://localhost:${appSettings.apiServerPort}`
-    },
-    stats: { colors: true }
   }
 };
