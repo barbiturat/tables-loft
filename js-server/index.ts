8e7f14e37d38aeeabda01fc21d3eb5bb419d90fd
vadimnekrasov <gdekefir@gmail.com>
2017-01-13 14:59:30 +0300

    experiments with server

--- js-server/index.ts
+++ js-server/index.ts
@@ -20,5 +20,6 @@ app.all('*', function(req, res){
 
 http.createServer(app)
   .listen(port, function () {
+    console.log('process.env', process.env);
     console.log(`JSON Server is running on port ${port}`);
   });
