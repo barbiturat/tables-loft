c463a69638a70b52e1565791d470b19837297ad5
vadimnekrasov <gdekefir@gmail.com>
2017-01-14 11:39:21 +0300

    experiments with server

--- js-server/index.ts
+++ js-server/index.ts
@@ -15,6 +15,7 @@ function pathFromRoot(url = '') {
 app.use(express.static(publicPath));
 
 app.all('*', function(req, res){
+  console.log('req', req);
   res.sendfile(`${publicPath}/index.html`)
 });
 
