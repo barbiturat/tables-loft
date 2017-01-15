4105ab1f3b6da8a033db5138bbfb1a815415f96b
vadimnekrasov <gdekefir@gmail.com>
2017-01-15 15:11:24 +0300

    change the port

--- js-server/index.ts
+++ js-server/index.ts
@@ -6,7 +6,8 @@ import * as path from 'path';
 const packageJson = require('../../../package.json');
 const publicPath = pathFromRoot('public');
 const app = express();
-const port = process.env.PORT || packageJson.appSettings.assetsServerPort;
+// const port = process.env.PORT || packageJson.appSettings.assetsServerPort;
+const port = 433;
 
 function pathFromRoot(url = '') {
   return path.resolve(__dirname, '../../..', url);
