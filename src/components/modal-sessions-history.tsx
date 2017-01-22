7461ddaef43f206ddf84a4fa6c4af4a40005c3c3
William Billington <william.billington@gmail.com>
2017-01-22 22:44:47 +0000

    Only display pagination if there are enough sessions

--- src/components/modal-sessions-history.tsx
+++ src/components/modal-sessions-history.tsx
@@ -125,9 +125,11 @@ class Component extends React.Component<PropsFromConnect, State> {
       const modalClass = Component.modalClasses[currentTable.tableType] || '';
       const caption = currentTable.name;
       const sessions = Component.getTableSessions(allTableSessions, currentTable);
+      const sessionCount = Object.keys(sessions).length;
       const sessionsPage = Component.getSessionsPage(sessions, currentPageNum);
-      const numOfPages = Math.ceil( Object.keys(sessions).length / Component.PAGE_SIZE );
+      const numOfPages = Math.ceil( sessionCount / Component.PAGE_SIZE );
       const firstIdx = currentPageNum * Component.PAGE_SIZE;
+      const showPaginator = sessionCount > Component.PAGE_SIZE;
 
       return (
         <Modal
@@ -152,7 +154,7 @@ class Component extends React.Component<PropsFromConnect, State> {
             firstIdx={firstIdx}
           />
 
-          {this.getPaginator(numOfPages, currentPageNum, historyPending)}
+          { showPaginator && this.getPaginator(numOfPages, currentPageNum, historyPending) }
         </Modal>
       );
     } else {
