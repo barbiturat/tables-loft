080cfefcad363dcf4042798a1eb1da75b12c8046
vadimnekrasov <gdekefir@gmail.com>
2017-01-23 02:11:56 +0300

    remove "PENDING_REQUEST_TABLE_STATUS_CHANGED" action

--- src/epics/index.ts
+++ src/epics/index.ts
@@ -3,7 +3,6 @@ import {combineEpics} from 'redux-observable';
 import formValidation from './request-login';
 import fetchTables from './fetch-tables';
 import startTable from './start-table';
-import setTablePending from './set-table-pending';
 import stopTable from './stop-table';
 import updateUtcMilliseconds from './update-utc-milliseconds';
 import requestAdminToken from './request-admin-token';
@@ -17,7 +16,6 @@ export default combineEpics(
   fetchTables,
   fetchSessionsHistory,
   startTable,
-  setTablePending,
   updateUtcMilliseconds,
   stopTable,
   requestTableSessionChange,
