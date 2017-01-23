c1d0838931f5a9c3b2a8790b8ce39a2515dcc031
vadimnekrasov <gdekefir@gmail.com>
2017-01-23 21:25:11 +0300

    add pipes to the fetchTables epic

--- src/epics/fetch-tables.ts
+++ src/epics/fetch-tables.ts
@@ -1,5 +1,6 @@
 import {Observable, AjaxError} from 'rxjs';
 import {Epic} from 'redux-observable';
+import {pipe} from 'ramda';
 
 import {FETCHING_TABLES} from '../constants/action-names';
 import {get, isAjaxResponseDefined, getRequestFailedAction} from '../helpers/requests';
@@ -7,12 +8,12 @@ import {ResponseTablesPayload} from '../interfaces/api-responses';
 import {AjaxResponseTyped, AjaxResponseDefined} from '../interfaces/index';
 import pendingTables from '../action-creators/pending-tables';
 import {urlTables} from '../constants/urls';
-import {SimpleAction} from '../interfaces/actions';
+import {SimpleAction, ActionWithPayload} from '../interfaces/actions';
 import {tablesToFront, tableSessionsToFront} from '../helpers/api-data-converters/index';
 import {TableSession, Table as TableBackend} from '../interfaces/backend-models';
 import changingTableSessions from '../action-creators/changing-table-sessions';
 import changingTables from '../action-creators/changing-tables';
-import {Tables} from '../interfaces/store-models';
+import {Tables, TableSessions} from '../interfaces/store-models';
 import {API_URL} from '../constants/index';
 
 type ResponseOk = AjaxResponseTyped<ResponseTablesPayload>;
@@ -39,12 +40,18 @@ const fetchTables = ((action$) => {
             .mergeMap((ajaxData: ResponseOk | AjaxError) => {
               if ( isAjaxResponseDefined<ResponseOkDefined>(ajaxData) ) {
                 const tables = ajaxData.response.tables;
-                const tableSessions = getTableSessionsFromTables(tables);
-                const convertedTables: Tables = tablesToFront(tables);
-                const convertedTableSessions = tableSessionsToFront(tableSessions);
 
-                const setTables = changingTables(convertedTables);
-                const setTableSessions = changingTableSessions(convertedTableSessions);
+                const setTableSessions = pipe< TableBackend[], TableSession[], TableSessions, ActionWithPayload<TableSessions> >(
+                  getTableSessionsFromTables,
+                  tableSessionsToFront,
+                  changingTableSessions
+                )(tables);
+
+                const setTables = pipe< TableBackend[], Tables, ActionWithPayload<Tables> >(
+                  tablesToFront,
+                  changingTables
+                )(tables);
+
                 const tablesPendingStop = pendingTables(false);
 
                 return Observable.of<any>(
@@ -54,7 +61,6 @@ const fetchTables = ((action$) => {
                 );
               } else {
                 const fetchFailedAction = getRequestFailedAction(ajaxData.status, 'Fetching tables error');
-
                 const tablesPendingStopAction = pendingTables(false);
 
                 return Observable.of<any>(
