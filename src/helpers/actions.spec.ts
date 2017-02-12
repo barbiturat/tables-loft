5a028d454eb1e788e88005d99605d612785ab706
vadimnekrasov <gdekefir@gmail.com>
2017-02-13 01:26:01 +0300

    refactor all the tests for jest

--- src/helpers/actions.spec.ts
+++ src/helpers/actions.spec.ts
@@ -1,44 +1,39 @@
-describe('my beverage 111', () => {
-  test('is delicious', () => {
-    expect(true).toBeTruthy();
+import {createSimpleAction, createActionWithPayload} from './actions';
+import {SimpleAction} from '../interfaces/actions';
+
+describe('createSimpleAction', () => {
+  test('it should be defined', () => {
+    expect(createSimpleAction).toBeDefined();
+  });
+
+  test('returns proper action', () => {
+    const actionType = 'TEST_TYPE';
+    const action: SimpleAction = createSimpleAction(actionType);
+
+    expect(action).toEqual({
+      type: actionType
+    });
   });
 });
 
-// import { expect } from 'chai';
-//
-// import {createSimpleAction, createActionWithPayload} from './actions';
-// import {SimpleAction} from '../interfaces/actions';
-//
-// describe('createSimpleAction: ', () => {
-//   it('should be defined', () => {
-//     expect(createSimpleAction).to.exist;
-//   });
-//
-//   it('returns proper action', () => {
-//     const actionType = 'TEST_TYPE';
-//     const action: SimpleAction = createSimpleAction(actionType);
-//
-//     action.should.be.deep.equal({
-//       type: actionType
-//     });
-//   });
-// });
-//
-// describe('createSimpleAction: ', () => {
-//   it('should be defined', () => {
-//     expect(createActionWithPayload).to.exist;
-//   });
-//
-//   it('returns proper action', () => {
-//     const actionType = 'TEST_TYPE';
-//     const payload = {
-//       someData: 100
-//     };
-//     const action: SimpleAction = createActionWithPayload(actionType, payload);
-//
-//     action.should.be.deep.equal({
-//       type: actionType,
-//       payload
-//     });
-//   });
-// });
+describe('createSimpleAction', () => {
+  test('should be defined', () => {
+    expect(createActionWithPayload).toBeDefined();
+  });
+
+  test('returns proper action', () => {
+    const actionType = 'TEST_TYPE';
+    const payload = {
+      someData: 100
+    };
+    const action: SimpleAction = createActionWithPayload(actionType, payload);
+
+    expect(action).toEqual({
+      type: actionType,
+      payload
+    });
+  });
+});
+
+
+
