444005884901583c90a124fd733b2987b29b2fed
vadimnekrasov <gdekefir@gmail.com>
2017-04-07 22:10:16 +0300

    fix snapshot testing

--- src/components/table-timer.spec.tsx
+++ src/components/table-timer.spec.tsx
@@ -1,16 +1,24 @@
 import * as React from 'react';
 import * as renderer from 'react-test-renderer';
+import {Action} from 'redux';
 
 import {Component as TableTimerComponent} from './table-timer';
 
 test('TableTimer renders correctly', () => {
-  const now = 1491581751312;
+  const startsAt = 1491581751312;
+  const utcMilliseconds = 1491581751312;
   const isActive = true;
+
+  const props = {
+    isActive,
+    utcMilliseconds,
+    startsAt,
+    dispatch: (action: Action) => action
+  };
 
   const tree = (renderer as any).create(
     <TableTimerComponent
-      isActive={isActive}
-      startsAt={now}
+      {...props}
     />
   ).toJSON();
 
