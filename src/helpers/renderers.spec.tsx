49e388cf9a14bfbd8ea6bf628d0d43de558b7ec5
vadimnekrasov <gdekefir@gmail.com>
2017-04-17 21:19:23 +0300

    add a test

--- src/helpers/renderers.spec.tsx
+++ src/helpers/renderers.spec.tsx
@@ -1,5 +1,26 @@
-describe('my beverage 444', () => {
-  test('is delicious', () => {
-    expect(true).toBeTruthy();
+import * as React from 'react';
+import * as renderer from 'react-test-renderer';
+import {shallow} from 'enzyme';
+import {FieldState} from 'react-redux-form';
+
+import {renderErrorsBlock} from './renderers';
+
+describe('renderErrorsBlock', () => {
+  it('renders block with passed children', () => {
+    const childrenEl = (<div id="someEl">
+      <span className="someChild">111</span>
+    </div>);
+
+    const resultBlock = renderErrorsBlock({
+      model: '',
+      modelValue: '',
+      fieldValue: {} as FieldState,
+      children: childrenEl
+    });
+
+    const component = shallow(resultBlock);
+
+    expect(component.childAt(0).is('div#someEl')).toBe(true);
+    expect(component.childAt(0).childAt(0).is('span.someChild')).toBe(true);
   });
 });
