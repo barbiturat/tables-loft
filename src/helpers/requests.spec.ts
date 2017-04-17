be85714b56ea6c5d8de16b7df4f3bebba42723c5
vadimnekrasov <gdekefir@gmail.com>
2017-04-17 13:19:14 +0300

    add test

--- src/helpers/requests.spec.ts
+++ src/helpers/requests.spec.ts
@@ -3,6 +3,7 @@ jest.mock('./process-env');
 import Mock = jest.Mock;
 import {AjaxError, AjaxRequest, TestScheduler, Observable} from 'rxjs';
 import {equals} from 'ramda';
+import * as queryString from 'query-string';
 import * as jsc from 'jsverify';
 // tslint:disable-next-line:no-require-imports
 require('./testing/jasmineHelpers2'); // https://github.com/jsverify/jsverify#usage-with-jasmine
@@ -137,24 +138,27 @@ describe('get', () => {
     expect(handleErrorFirstCall).toEqual([errorData]);
   });
 
-  /*it(`Observable.ajax.get is called with data from get's "dataToSend" argument`, () => {
-    Observable.ajax.get = jest.fn(() => Observable.of(null));
+  jsc.property(`DependencyContainer.ajaxGet is called with data from get's "dataToSend" argument`,
+    jsc.dict(jsc.string),
+    alphanumericSymbolsArb(20, '---...'),
+    alphanumericSymbolsArb(20, '-'),
+    (dataToSend: {}, siteName: string, apiKey: string) => {
+      (getProcessEnv as Mock<any>).mockImplementationOnce(() => ({
+        API_KEY: apiKey
+      }));
+      DependencyContainer.ajaxGet = jest.fn(() => Observable.of(null));
 
-    const dataToSend = {
-      field1: 'some value',
-      field2: 222
-    };
-    const serializedData = queryString.stringify(dataToSend);
-    const url = 'http://some-url.com';
-    const urlWithQueryString = `${url}?${serializedData}`;
+      const serializedData = queryString.stringify(dataToSend);
+      const url = `http://${siteName}.com`;
+      const urlWithQueryString = `${url}${serializedData ? '?' : ''}${serializedData}`;
 
-    get('http://some-url.com', dataToSend);
+      get(url, dataToSend);
 
-    const firstCall = (Observable.ajax.get as Mock<any>).mock.calls[0];
-    const firstArg = firstCall[0];
+      const firstCall = (DependencyContainer.ajaxGet as Mock<any>).mock.calls[0];
+      const firstArg = firstCall[0];
 
-    expect(firstArg).toEqual(urlWithQueryString);
-  });*/
+      return equals(firstArg)(urlWithQueryString);
+  });
 
 /*
   it(`Observable.ajax.get is called with data from get's "headers" argument`, () => {
