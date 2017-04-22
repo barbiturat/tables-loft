49417bda5c39d5f96a1be24f0874a551bb9fcaaa
vadimnekrasov <gdekefir@gmail.com>
2017-04-22 20:04:03 +0300

    remove unused files

--- src/action-creators/fetching-admin-token.ts
+++
@@ -1,6 +0,0 @@
-import {FETCHING_ADMIN_TOKEN} from '../constants/action-names';
-import {createSimpleAction} from '../helpers/actions';
-
-const fetchingAdminToken = createSimpleAction(FETCHING_ADMIN_TOKEN);
-
-export default fetchingAdminToken;
