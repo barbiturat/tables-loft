1c339e43f509cf4c8fe540595179ba828176611e
vadimnekrasov <gdekefir@gmail.com>
2017-04-22 15:03:51 +0300

    add di

--- src/helpers/testing/expect-epic.spec.ts
+++ src/helpers/testing/expect-epic.spec.ts
@@ -4,41 +4,51 @@ import {Observable} from 'rxjs';
 
 import {expectEpic} from './expect-epic';
 
-const actions = {
-  FETCH_FOO: 'FETCH_FOO',
-  FETCH_FOO_FULFILLED: 'FETCH_FOO_FULFILLED',
-  FETCH_FOO_CANCELLED: 'FETCH_FOO_CANCELLED',
-  FETCH_FOO_REJECTED: 'FETCH_FOO_REJECTED',
-};
-
-const ajaxDependencies = {
-  getAjax: null
-};
-
-const testEpic: Epic<{payload: {}}, {}> = (action$, store) => {
-  return action$.ofType(actions.FETCH_FOO)
-    .switchMap(action => {
-      const dataToSend = action.payload;
-
-      return ajaxDependencies.getAjax('some-url', dataToSend)
-        .takeUntil(action$.ofType(actions.FETCH_FOO_CANCELLED))
-        .map(payload => ({
-          type: actions.FETCH_FOO_FULFILLED,
-          payload: payload
-        }))
-        .catch(error => Observable.of({
-          type: actions.FETCH_FOO_REJECTED,
-          payload: error.xhr.response,
-          error: true
-        }));
-    });
-};
-
 describe('expectEpic', () => {
-  it('calls the correct API', () => {
-    const responseData = {id: 123, name: 'Bilbo'};
 
-    expectEpic(testEpic, {
+  const dataProvider = (actions: Record<'FETCH_FOO' | 'FETCH_FOO_FULFILLED' | 'FETCH_FOO_CANCELLED' | 'FETCH_FOO_REJECTED', string>, url) => {
+    const getEpic = (getAjax: (url: string, dataToSend: any) => Observable<any>): Epic<{payload: {}}, {}> => {
+      return (action$, store) => {
+        return action$.ofType(actions.FETCH_FOO)
+          .switchMap(action => {
+            const dataToSend = action.payload;
+
+            return getAjax(url, dataToSend)
+              .takeUntil(action$.ofType(actions.FETCH_FOO_CANCELLED))
+              .map(payload => ({
+                type: actions.FETCH_FOO_FULFILLED,
+                payload: payload
+              }))
+              .catch(error => Observable.of({
+                type: actions.FETCH_FOO_REJECTED,
+                payload: error.xhr.response,
+                error: true
+              }));
+          });
+      };
+    };
+
+    return {
+      actions,
+      url,
+      getEpic
+    };
+  };
+
+  it('calls the correct API', () => {
+    const testData = dataProvider(
+      {
+        FETCH_FOO: 'FETCH_FOO',
+        FETCH_FOO_FULFILLED: 'FETCH_FOO_FULFILLED',
+        FETCH_FOO_CANCELLED: 'FETCH_FOO_CANCELLED',
+        FETCH_FOO_REJECTED: 'FETCH_FOO_REJECTED'
+      },
+      'some-url'
+    );
+    const responseData = {id: 123, name: 'Bilbo'};
+    const {actions, url, getEpic} = testData;
+
+    expectEpic(getEpic, {
       action: {
         marbles: '(a|)',
         values: {
@@ -63,8 +73,7 @@ describe('expectEpic', () => {
           a: responseData
         }
       },
-      callAjaxArgs: ['some-url', {id: 123}],
-      dependenciesObj: ajaxDependencies
+      callAjaxArgs: [url, {id: 123}]
     });
   });
 
--- src/helpers/testing/expect-epic.ts
+++ src/helpers/testing/expect-epic.ts
@@ -5,29 +5,25 @@ import {MiddlewareAPI} from 'redux';
 import configureMockStore from 'redux-mock-store';
 
 import {ActionWithPayload, SimpleAction} from '../../interfaces/actions';
-import {Dict, Writable} from '../../interfaces/index';
+import {Dict} from '../../interfaces/index';
 
-interface TestObservableData <T>{
+interface TestObservableData <T> {
   readonly marbles: string;
   readonly values?: Dict<T>;
   readonly error?: any;
 }
 
-interface AjaxDependencies {
-  readonly getAjax: () => Observable<any>;
-}
-
 const mockStore = configureMockStore();
 
-export const expectEpic = (epic: Epic<any, any>, options: {
-  action: TestObservableData< SimpleAction | ActionWithPayload<any> >,
-  expected: TestObservableData< SimpleAction | ActionWithPayload<any> >,
-  response: TestObservableData<any>,
-  store?: MiddlewareAPI<any>
-  callAjaxArgs: ReadonlyArray<any>,
-  dependenciesObj?: Writable<AjaxDependencies>
-}) => {
-  const {action, expected, response, callAjaxArgs, dependenciesObj} = options;
+export const expectEpic = (getEpic: (getAjax: (url: string, dataToSend: any) => Observable<any>) => Epic<any, any>,
+                           options: {
+                             action: TestObservableData<SimpleAction | ActionWithPayload<any>>,
+                             expected: TestObservableData<SimpleAction | ActionWithPayload<any>>,
+                             response: TestObservableData<any>,
+                             store?: MiddlewareAPI<any>
+                             callAjaxArgs: ReadonlyArray<any>
+                           }) => {
+  const {action, expected, response, callAjaxArgs} = options;
   const store = options.store || mockStore();
   const testScheduler = new TestScheduler((actual: any, expectedData: any) => {
     return expect(actual).toEqual(expectedData);
@@ -37,11 +33,10 @@ export const expectEpic = (epic: Epic<any, any>, options: {
     testScheduler.createHotObservable<{}>(action.marbles, action.values, action.error)
   );
   const response$ = testScheduler.createColdObservable(response.marbles, response.values, response.error);
-
-  dependenciesObj.getAjax = jest.fn(() => response$);
-
-  const callsOfGetAjax = (dependenciesObj.getAjax as Mock<any>).mock.calls;
+  const getAjax = jest.fn(() => response$);
+  const callsOfGetAjax = (getAjax as Mock<any>).mock.calls;
   const responseMarbles = '^!';
+  const epic = getEpic(getAjax);
   const test$ = epic(action$, store);
 
   testScheduler.expectObservable(test$).toBe(expected.marbles, expected.values);
