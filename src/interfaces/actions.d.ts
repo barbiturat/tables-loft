3402894df73109d4a8544c1199a53d0b8c53801d
vadimnekrasov <gdekefir@gmail.com>
2017-08-16 03:12:46 +0300

    remove unnecessary interface

--- src/interfaces/actions.d.ts
+++ src/interfaces/actions.d.ts
@@ -1,11 +1,5 @@
 import {Action, BaseAction} from 'redux-actions';
 
-export interface ActionWithPayload<TPayload> extends BaseAction {
-  readonly payload: TPayload;
-  readonly type: string;
-  readonly [key: string]: any;
-}
-
 interface FormSubmitActionPayload {
   readonly formModelPath: string;
   readonly formData: any;
