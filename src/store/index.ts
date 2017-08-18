e7e8f28d5ae252329df02e0670ae284466a3c54c
vadimnekrasov <gdekefir@gmail.com>
2017-08-18 03:07:45 +0300

    update packages

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,4 +1,4 @@
-import { createStore, compose, applyMiddleware, Store } from 'redux';
+import { createStore, compose, applyMiddleware, Store, Action } from 'redux';
 import { reduxReactRouter } from 'redux-router';
 import { createHistory } from 'history';
 import { createEpicMiddleware, EpicMiddleware } from 'redux-observable';
@@ -8,9 +8,10 @@ import epics from '../epics/index';
 import allReducers from '../reducers/index';
 import { StoreStructure } from '../interfaces/store-models';
 
-const epicMiddleware: EpicMiddleware<{}, StoreStructure> = createEpicMiddleware(
-  epics
-);
+const epicMiddleware: EpicMiddleware<
+  Action,
+  StoreStructure
+> = createEpicMiddleware(epics);
 const composeEnhancers =
   (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
