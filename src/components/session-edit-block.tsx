c0480e9e221a48f7df4d13fc5220f04c53753b7b
vadimnekrasov <gdekefir@gmail.com>
2017-09-01 06:50:09 +0300

    make component functional

--- src/components/session-edit-block.tsx
+++ src/components/session-edit-block.tsx
@@ -2,6 +2,8 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import KeyboardEvent = React.KeyboardEvent;
 import * as moment from 'moment';
+import { compose, lifecycle, withHandlers, withState } from 'recompose';
+import * as R from 'ramda';
 
 import { StoreStructure } from '../interfaces/store-models';
 import { PropsExtendedByConnect } from '../interfaces/component';
@@ -19,146 +21,130 @@ interface Props {
   readonly onEditComplete: () => void;
 }
 
-interface State {
-  readonly hours: number;
-  readonly minutes: number;
-}
-
 interface MappedProps {}
 
 type PropsFromConnect = PropsExtendedByConnect<Props, MappedProps>;
 
-const getSessionDurationData = (
-  durationSeconds: number
-): SessionDurationData => {
-  const duration = moment.duration({ seconds: durationSeconds });
+const getSessionDurationData = R.memoize(
+  (durationSeconds: number): SessionDurationData => {
+    const duration = moment.duration({ seconds: durationSeconds });
 
-  return {
-    hours: Math.floor(duration.asHours()),
-    minutes: Math.floor(duration.minutes())
-  };
-};
-
-class Component extends React.Component<PropsFromConnect, State> {
-  constructor(props: PropsFromConnect) {
-    super(props);
-
-    const durationData = getSessionDurationData(props.durationSeconds);
-
-    this.state = {
-      hours: durationData.hours,
-      minutes: durationData.minutes
+    return {
+      hours: Math.floor(duration.asHours()),
+      minutes: Math.floor(duration.minutes())
     };
   }
+);
 
-  componentWillReceiveProps(nextProps: PropsFromConnect) {
-    if (nextProps.durationSeconds !== this.props.durationSeconds) {
-      const durationData = getSessionDurationData(nextProps.durationSeconds);
-      const { hours, minutes } = durationData;
+const enhance = compose(
+  withState(
+    'hours',
+    'setHours',
+    ({ durationSeconds }) =>
+      (getSessionDurationData(durationSeconds) as any).hours
+  ),
+  withState(
+    'minutes',
+    'setMinutes',
+    ({ durationSeconds }) =>
+      (getSessionDurationData(durationSeconds) as any).minutes
+  ),
+  withHandlers({
+    onInputChange: ({ setHours, setMinutes }) => (
+      event: ChangeEvent<HTMLInputElement>
+    ) => {
+      const nameEquals = R.useWith(R.call, [R.identical, R.prop('name')]);
+      const setTimePart = R.useWith(R.call, [
+        R.identity,
+        R.compose(Number, R.prop('value'))
+      ]);
 
-      this.setState({
-        hours,
-        minutes
-      });
-    }
-  }
-
-  onInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-    const input = event.currentTarget;
-
-    if (input.name === 'hours') {
-      this.setEditingHours(Number(input.value));
-    }
-    if (input.name === 'minutes') {
-      this.setEditingMinutes(Number(input.value));
-    }
-  };
-
-  onInputKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
-    const { dispatch, sessionId, onEditComplete } = this.props;
-
-    if (typeof sessionId !== 'number') {
-      return;
-    }
-
-    const isEsc = event.keyCode === 27;
-    const isEnter = event.keyCode === 13;
-
-    if (isEsc || isEnter) {
-      event.preventDefault();
-    }
-
-    if (isEnter) {
-      const duration = moment.duration({
-        hours: this.state.hours,
-        minutes: this.state.minutes
-      });
-      const newSeconds = duration.asSeconds();
-      const action = requestingTableSessionChange(sessionId, newSeconds);
-
-      dispatch(action);
-      onEditComplete();
-      return;
-    }
-
-    if (isEsc) {
-      onEditComplete();
-    }
-  };
-
-  setEditingHours(hours: number) {
-    this.setState({
-      ...this.state,
-      ...{
-        hours: hours
+      return R.cond([
+        [nameEquals('hours'), setTimePart(setHours)],
+        [nameEquals('minutes'), setTimePart(setMinutes)]
+      ])(event.currentTarget);
+    },
+    onInputKeyDown: ({
+      dispatch,
+      sessionId,
+      onEditComplete,
+      hours,
+      minutes
+    }) => (event: KeyboardEvent<HTMLInputElement>) => {
+      if (typeof sessionId !== 'number') {
+        return;
       }
-    });
-  }
 
-  setEditingMinutes(minutes: number) {
-    this.setState({
-      ...this.state,
-      ...{
-        minutes: minutes
+      const isEsc = event.keyCode === 27;
+      const isEnter = event.keyCode === 13;
+
+      if (isEsc || isEnter) {
+        event.preventDefault();
       }
-    });
-  }
 
-  render() {
-    const hr: string = String(this.state.hours);
-    const min: string = String(this.state.minutes);
+      if (isEnter) {
+        const duration = moment.duration({
+          hours,
+          minutes
+        });
+        const newSeconds = duration.asSeconds();
+        const action = requestingTableSessionChange(sessionId, newSeconds);
 
-    return (
-      <form className="session-edit-block__form">
-        <label className="session-edit-block__label">
-          <span className="session-edit-block__text">hr:</span>
-          <span className="session-edit-block__input-container">
-            <input
-              name="hours"
-              className="session-edit-block__input"
-              defaultValue={hr}
-              onChange={this.onInputChange}
-              onKeyDown={this.onInputKeyDown}
-            />
-          </span>
-        </label>
+        dispatch(action);
+        onEditComplete();
+        return;
+      }
 
-        <label className="session-edit-block__label">
-          <span className="session-edit-block__text">min:</span>
-          <span className="session-edit-block__input-container">
-            <input
-              name="minutes"
-              className="session-edit-block__input"
-              defaultValue={min}
-              onChange={this.onInputChange}
-              onKeyDown={this.onInputKeyDown}
-            />
-          </span>
-        </label>
-      </form>
-    );
-  }
-}
+      if (isEsc) {
+        onEditComplete();
+      }
+    }
+  }),
+  lifecycle({
+    componentWillReceiveProps(nextProps: PropsFromConnect) {
+      const changeStates = R.converge(R.T, [
+        R.compose((this.props as any).setHours, R.prop('hours')),
+        R.compose((this.props as any).setMinutes, R.prop('minutes'))
+      ]);
+      const updateStates = R.compose(changeStates, getSessionDurationData);
+
+      R.unless(R.identical(this.props.durationSeconds), updateStates)(
+        nextProps.durationSeconds
+      );
+    }
+  })
+);
+
+const Component = enhance(
+  ({ hours, minutes, onInputChange, onInputKeyDown }: any) =>
+    <form className="session-edit-block__form">
+      <label className="session-edit-block__label">
+        <span className="session-edit-block__text">hr:</span>
+        <span className="session-edit-block__input-container">
+          <input
+            name="hours"
+            className="session-edit-block__input"
+            defaultValue={String(hours)}
+            onChange={onInputChange}
+            onKeyDown={onInputKeyDown}
+          />
+        </span>
+      </label>
+
+      <label className="session-edit-block__label">
+        <span className="session-edit-block__text">min:</span>
+        <span className="session-edit-block__input-container">
+          <input
+            name="minutes"
+            className="session-edit-block__input"
+            defaultValue={String(minutes)}
+            onChange={onInputChange}
+            onKeyDown={onInputKeyDown}
+          />
+        </span>
+      </label>
+    </form>
+);
 
 const SessionEditBlock = connect<
   any,
